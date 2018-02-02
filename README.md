5aa23b8a1d690044349f02dca7e70a0526c6a405
Caio Gondim <me@caiogondim.com>
2018-02-02 15:00:24 -0500

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# can-track.js
+<h1 align="center">can-track.js</h1>
+
+<div align="center">
+ <a href="https://www.npmjs.com/package/can-track"><img src="https://img.shields.io/npm/v/can-track.svg" /></a>
+</div>
+
+<br>
+
+Polyfill for [doNotTrack](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/doNotTrack).
+
+## Usage
+
+`canTrack` returns `false` if the user opted-out for tracking; `true` otherwise.
+
+```js
+canTrack() // => Boolean
+```
+
+## Reference
+- [Do Not Track documentation](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/doNotTrack)
+
+---
+
+[caiogondim.com](https://caiogondim.com) &nbsp;&middot;&nbsp;
+GitHub [@caiogondim](https://github.com/caiogondim) &nbsp;&middot;&nbsp;
+Twitter [@caio_gondim](https://twitter.com/caio_gondim)
---
+++ test/index.js
@@ -0,0 +1,46 @@
+const canTrack = require('../lib')
+
+// Spec: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/doNotTrack
+
+test('return true if doNotTrack equals "1"', () => {
+  const win = {
+    navigator: {
+      doNotTrack: '1'
+    }
+  }
+  expect(canTrack(win)).toBe(false)
+})
+
+test('return true if doNotTrack equals "0"', () => {
+  const win = {
+    navigator: {
+      doNotTrack: '0'
+    }
+  }
+  expect(canTrack(win)).toBe(true)
+})
+
+test('return true if doNotTrack is undefined', () => {
+  const win = {
+    navigator: {
+      doNotTrack: undefined
+    }
+  }
+  expect(canTrack(win)).toBe(true)
+})
+
+test('compatibility with IE/Edge', () => {
+  const win = {
+    navigator: {
+      msDoNotTrack: "1"
+    }
+  }
+  expect(canTrack(win)).toBe(false)
+})
+
+test('compatibility with Safari', () => {
+  const win = {
+    doNotTrack: "1"
+  }
+  expect(canTrack(win)).toBe(false)
+})
